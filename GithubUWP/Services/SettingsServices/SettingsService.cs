bf66a2954eb091a1a5a45a48ab2cc971952a4c99
Prajjwal <prajjwaldimri@hotmail.com>
2016-11-11 01:12:02 +0530

    Added Template10 Hamburger

---
+++ GithubUWP/Services/SettingsServices/SettingsService.cs
@@ -0,0 +1,78 @@
+using System;
+using Template10.Common;
+using Template10.Utils;
+using Windows.UI.Xaml;
+
+namespace GithubUWP.Services.SettingsServices
+{
+    public class SettingsService
+    {
+        public static SettingsService Instance { get; } = new SettingsService();
+        Template10.Services.SettingsService.ISettingsHelper _helper;
+        private SettingsService()
+        {
+            _helper = new Template10.Services.SettingsService.SettingsHelper();
+        }
+
+        public bool UseShellBackButton
+        {
+            get { return _helper.Read<bool>(nameof(UseShellBackButton), true); }
+            set
+            {
+                _helper.Write(nameof(UseShellBackButton), value);
+                BootStrapper.Current.NavigationService.GetDispatcherWrapper().Dispatch(() =>
+                {
+                    BootStrapper.Current.ShowShellBackButton = value;
+                    BootStrapper.Current.UpdateShellBackButton();
+                });
+            }
+        }
+
+        public ApplicationTheme AppTheme
+        {
+            get
+            {
+                var theme = ApplicationTheme.Light;
+                var value = _helper.Read<string>(nameof(AppTheme), theme.ToString());
+                return Enum.TryParse<ApplicationTheme>(value, out theme) ? theme : ApplicationTheme.Dark;
+            }
+            set
+            {
+                _helper.Write(nameof(AppTheme), value.ToString());
+                (Window.Current.Content as FrameworkElement).RequestedTheme = value.ToElementTheme();
+                Views.Shell.HamburgerMenu.RefreshStyles(value, true);
+            }
+        }
+
+        public TimeSpan CacheMaxDuration
+        {
+            get { return _helper.Read<TimeSpan>(nameof(CacheMaxDuration), TimeSpan.FromDays(2)); }
+            set
+            {
+                _helper.Write(nameof(CacheMaxDuration), value);
+                BootStrapper.Current.CacheMaxDuration = value;
+            }
+        }
+
+        public bool ShowHamburgerButton
+        {
+            get { return _helper.Read<bool>(nameof(ShowHamburgerButton), true); }
+            set
+            {
+                _helper.Write(nameof(ShowHamburgerButton), value);
+                Views.Shell.HamburgerMenu.HamburgerButtonVisibility = value ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        public bool IsFullScreen
+        {
+            get { return _helper.Read<bool>(nameof(IsFullScreen), false); }
+            set
+            {
+                _helper.Write(nameof(IsFullScreen), value);
+                Views.Shell.HamburgerMenu.IsFullScreen = value;
+            }
+        }
+    }
+}
+
---
+++ GithubUWP/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using GithubUWP.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace GithubUWP.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
