18e8ff803246ba74286b6be79d85fa88146addd0
Prajjwal <prajjwaldimri@hotmail.com>
2016-11-14 22:18:37 +0530

    Created Intial Main-Page which shows user events

--- GithubUWP/ViewModels/MainPageViewModel.cs
+++ GithubUWP/ViewModels/MainPageViewModel.cs
@@ -1,54 +1,40 @@
 using Template10.Mvvm;
 using System.Collections.Generic;
 using System;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using Windows.Security.Credentials;
 using Template10.Services.NavigationService;
 using Windows.UI.Xaml.Navigation;
+using Octokit;
+using Template10.Utils;
 
 namespace GithubUWP.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel()
-        {
-            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
-            {
-                //TODO: Add design-time values
-            }
-        }
-
+        public ObservableCollection<Activity> FeedList { get; set; }
 
         public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
         {
-            if (suspensionState.Any())
+            //Initializing Octokit
+            var client = new GitHubClient(new ProductHeaderValue("githubuwp"));
+            var vault = new PasswordVault();
+            var passwordCredential = new PasswordCredential();
+            if (vault.FindAllByResource("GithubAccessToken") != null)
             {
+                passwordCredential = vault.Retrieve("GithubAccessToken", "Github");
             }
-            await Task.CompletedTask;
+            client.Credentials = new Credentials(passwordCredential.Password);
+
+            var userEvents = await client.Activity.Events.GetAllUserReceived(client.User.Current().Result.Login);
+            FeedList = userEvents.ToObservableCollection();
+            var gists = await client.Gist.GetAllForUser(client.User.Current().Result.Login);
+            //If in any case retrieves any unread notification remove it from the List.
+            
+            RaisePropertyChanged(String.Empty);
         }
-
-        public override async Task OnNavigatedFromAsync(IDictionary<string, object> suspensionState, bool suspending)
-        {
-            if (suspending)
-            {
-            }
-            await Task.CompletedTask;
-        }
-
-        public override async Task OnNavigatingFromAsync(NavigatingEventArgs args)
-        {
-            args.Cancel = false;
-            await Task.CompletedTask;
-        }
-
-        public void GotoSettings() =>
-            NavigationService.Navigate(typeof(Views.SettingsPage), 0);
-
-        public void GotoPrivacy() =>
-            NavigationService.Navigate(typeof(Views.SettingsPage), 1);
-
-        public void GotoAbout() =>
-            NavigationService.Navigate(typeof(Views.SettingsPage), 2);
 
     }
 }
