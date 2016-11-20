61dba9b427955582ea221a20309f9fd889b4cbb3
Prajjwal <prajjwaldimri@hotmail.com>
2016-11-20 22:57:50 +0530

    Fixed incorrect event types showing on Activities Page

--- GithubUWP/Services/Converters/EventToSentenceConverter.cs
+++ GithubUWP/Services/Converters/EventToSentenceConverter.cs
@@ -1,31 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
+using GithubUWP.ViewModels;
+using Octokit;
 
 namespace GithubUWP.Services.Converters
 {
-    public class EventToSentenceConverter : IValueConverter
+    public class EventToSentenceConverter : DependencyObject,IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            var eventName = value.ToString();
+       {
+            var currentActivity = value as Activity;
+            var eventName = currentActivity.Type;
             var eventSentence = "";
             switch (eventName)
             {
                 case "IssuesEvent":
-                    eventSentence = "opened an issue at";
+                    var issueEventPayload = currentActivity.Payload as IssueEventPayload;
+                    eventSentence = issueEventPayload.Action +" an issue at ";
                     break;
                 case "ForkEvent":
-                    eventSentence = "forked";
+                    eventSentence = " forked ";
                     break;
                 case "WatchEvent":
-                    eventSentence = "starred";
+                    eventSentence = " starred ";
                     break;
                 case "PushEvent":
-                    eventSentence = "pushed to";
+                    eventSentence = " pushed to ";
                     break;
                 default:
                     eventSentence = "";
