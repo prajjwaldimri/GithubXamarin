0e4353b64ec6b168eee2448aeb22db2d6782b1ae
Prajjwal <prajjwaldimri@hotmail.com>
2016-12-03 23:30:01 +0530

    Raw layout for issue page #5

--- GithubUWP/ViewModels/IssuePageViewModel.cs
+++ GithubUWP/ViewModels/IssuePageViewModel.cs
@@ -12,14 +12,41 @@ namespace GithubUWP.ViewModels
 {
     public class IssuePageViewModel : ViewModelBase
     {
+        public int IssueNumber { get; set; }
         public string IssueName { get; set; }
+        public string IssueState { get; set; }
+        public string IssueTitle { get; set; }
+        public string IssueBody { get; set; }
+        public DateTime IssueCreatedAt { get; set; }
+        public User IssueCreator { get; set; }
+        public User AssignedUser { get; set; }
+        public DateTime ClosedAt { get; set; }
+        public User ClosedBy { get; set; }
+        public int CommentsCount { get; set; }
+        public IssueComment IssueComments { get; set; }
 
         public override Task OnNavigatedToAsync(object parameter, NavigationMode mode,
             IDictionary<string, object> state)
         {
             var issue = SessionState.Get<Issue>(parameter.ToString());
+            IssueNumber = issue.Number;
             IssueName = issue.Title;
+            IssueState = issue.State.ToString();
+            IssueTitle = issue.Title;
+            IssueBody = issue.Body;
+            AssignedUser = issue.User;
+            if (issue.State == ItemState.Closed)
+            {
+                // ReSharper disable once PossibleInvalidOperationException
+                // Checked above ^ already
+                ClosedAt = issue.ClosedAt.Value.DateTime;
+                ClosedBy = issue.ClosedBy;
+            }
+            IssueCreatedAt = issue.CreatedAt.DateTime;
+            IssueCreator = issue.User;
+            CommentsCount = issue.Comments;
             RaisePropertyChanged(string.Empty);
+            SessionState.Remove(parameter.ToString());
             return Task.CompletedTask;
         }
     }
