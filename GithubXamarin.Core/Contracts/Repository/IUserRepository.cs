314a84c22a52a23d58b1b5a0445ccfbf9bd32a4f
Prajjwal <prajjwaldimri@hotmail.com>
2017-01-11 23:11:17 +0530

    Added UserRepository #36

---
+++ GithubXamarin.Core/Contracts/Repository/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Octokit;
+using System.Threading.Tasks;
+
+namespace GithubXamarin.Core.Contracts.Repository
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUser(string userLoginId, GitHubClient gitHubClient);
+        Task<User> GetCurrentUser(GitHubClient authorizedGitHubClient);
+        Task<IEnumerable<User>> SearchUsers(string searchTerm, GitHubClient gitHubClient);
+        Task<IEnumerable<User>> GetCollaboratorsOfRepository(long repositoryId, GitHubClient gitHubClient);
+        Task<IEnumerable<User>> GetStargazersOfRepository(long repositoryId, GitHubClient gitHubClient);
+        
+    }
+}
---
+++ GithubXamarin.Core/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GithubXamarin.Core.Contracts.Repository;
+using Octokit;
+
+namespace GithubXamarin.Core.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public async Task<User> GetUser(string userLoginId, GitHubClient gitHubClient)
+        {
+            var usersClient = new UsersClient(new ApiConnection(gitHubClient.Connection));
+            return await usersClient.Get(userLoginId);
+        }
+
+        /// <summary>
+        /// Gets all the details for the current logged in user.
+        /// </summary>
+        /// <param name="authorizedGitHubClient"></param>
+        /// <returns></returns>
+        public async Task<User> GetCurrentUser(GitHubClient authorizedGitHubClient)
+        {
+            return await authorizedGitHubClient.User.Current();
+        }
+
+        public async Task<IEnumerable<User>> SearchUsers(string searchTerm, GitHubClient gitHubClient)
+        {
+            var searchClient = new SearchClient(new ApiConnection(gitHubClient.Connection));
+            var searchResult = await searchClient.SearchUsers(new SearchUsersRequest(searchTerm));
+            return searchResult.Items;
+        }
+
+        public async Task<IEnumerable<User>> GetCollaboratorsOfRepository(long repositoryId, GitHubClient gitHubClient)
+        {
+            var repoCollaboratorsClient = new RepoCollaboratorsClient(new ApiConnection(gitHubClient.Connection));
+            return await repoCollaboratorsClient.GetAll(repositoryId);
+        }
+
+        public async Task<IEnumerable<User>> GetStargazersOfRepository(long repositoryId, GitHubClient gitHubClient)
+        {
+            var starredClient = new StarredClient(new ApiConnection(gitHubClient.Connection));
+            return await starredClient.GetAllStargazers(repositoryId);
+        }
+        
+    }
+}
