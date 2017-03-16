ce6b2a5d04ca7e7a4802b262f08443859c2978a2
Prajjwal <prajjwaldimri@hotmail.com>
2017-03-16 22:09:39 +0530

    Fixed NewIssueView.xaml.cs

--- GithubXamarin.UWP/Views/NewIssueView.xaml.cs
+++ GithubXamarin.UWP/Views/NewIssueView.xaml.cs
@@ -1,26 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+﻿using MvvmCross.WindowsUWP.Views;
 
 namespace GithubXamarin.UWP.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class NewIssueView : Page
+    [MvxRegion("MainFrame")]
+    public sealed partial class NewIssueView : MvxWindowsPage
     {
         public NewIssueView()
         {
